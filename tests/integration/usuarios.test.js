dd633a1f3830a80fef531d2e67d0b86fbe7a01c3
Wako <dulo.rodrigo@gmail.com>
2019-06-10 22:00:47 -0500

    Pasa prueba de ID

--- tests/integration/usuarios.test.js
+++ tests/integration/usuarios.test.js
@@ -12,7 +12,20 @@ describe('/ususarios', () => {
   describe('GET /', () => {
     it('Debe regresar todos los usuarios', async () => {
       const res = await request(server).get('/usuarios')
+      const { usuarios } = res.body
+      expect(usuarios.length).toBeGreaterThan(0)
       expect(res.status).toBe(200)
+    })
+  })
+  describe('GET /:id', () => {
+    const n = usuarios.length
+    it('Debe regresar un usuario por ID', async () => {
+      const res = await request(server).get(`/usuarios/${n}`)
+      expect(res.status).toBe(200)
+    })
+    it('Debe buscar un ID que no existe', async () => {
+      const res = await request(server).get(`/usuarios/${n + 2}`)
+      expect(res.status).toBe(404)
     })
   })
 })
